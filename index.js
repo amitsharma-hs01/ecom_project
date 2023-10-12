496b7b987471494e7a63572cecc4f71434ab02c5
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 16:36:12 +0000

    tested

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
 
-//import path from "path"
-// import { fileURLToPath } from "url"
-// import { dirname } from "path"
+import path from "path"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
 
 dotenv.config()
 const app = express()
 const port=process.env.PORT||5000
-// const __filename=fileURLToPath(import.meta.url);
-// const __dirname=dirname(__filename)
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=dirname(__filename)
 
-// app.use(express.static(path.join(__dirname,"./client/build")))
-// app.use("*",function(req,res){
-//     res.sendFile(path.join(__dirname,"./client/build/index.html"))
-// })
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send("hey")
