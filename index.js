c0f66dac1b1e29a00072bb644dd7b88cf87bbf17
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-12 16:43:57 +0000

    test2

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import express from "express"
-import path from "path"
- import { fileURLToPath } from "url"
- import { dirname } from "path"
 import dotenv from "dotenv"
-
 import path from "path"
 import { fileURLToPath } from "url"
 import { dirname } from "path"
