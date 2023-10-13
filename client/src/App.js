66170e971cbc0a7d26a1eb567ebdc2c5d2196cef
amitsharma-hs01 hs <amitsharma.hs01@gmail.com>
2023-10-13 09:37:44 +0000

    basic layout build

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Layout from './components/Layout';
 function App() {
   return (
     <Layout>
-      <div>yo yo yo</div>
+      <div>yo yo yo {process.env.REACT_APP_ID}</div>
     </Layout>
   );
 }
